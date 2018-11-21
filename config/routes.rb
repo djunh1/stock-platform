6cf03f97b0fb2fae3e95dd1951c34240ca1d9183
djunh1 <djunh1@gmail.com>
2018-11-20 20:16:52 -0500

    Add portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
 
   get 'pages/about'
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   get 'pages/contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
