f8a4dcc0d398082785d8d6c591a3efb2e1630830
djunh1 <djunh1@gmail.com>
2018-11-28 18:57:30 -0500

    Add Edit and Update to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,6 +17,20 @@ class PortfoliosController < ApplicationController
         format.html {render :new}
       end
     end
+  end
 
+  def update
+    respond_to do |format|
+      @portfolio_item = Portfolio.find(params[:id])
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
