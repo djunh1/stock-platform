8d3dbd0bd38e29af4abd86d5d11ebdef3cd610c8
djunh1 <djunh1@gmail.com>
2018-11-28 19:44:58 -0500

    Add delete functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,14 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item deleted.' }
+    end
+  end
+
+
 end
