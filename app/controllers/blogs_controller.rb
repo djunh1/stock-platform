b2d92c1ab137dffcafbb151ced449c0202f967b5
djunh1 <djunh1@gmail.com>
2018-11-28 21:09:57 -0500

    add status to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -56,6 +56,16 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
     end
+  end
+
+  def toggle_status
+
+      if @blog.draft?
+        @blog.published!
+      elsif @blog.published?
+        @blog.draft!
+      end
+      redirect_to blogs_url, notice: 'Report Status Updated!'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
