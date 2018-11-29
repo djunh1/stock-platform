0969479f13607b11916a5149e6a57a9167a8d01d
djunh1 <djunh1@gmail.com>
2018-11-29 18:11:18 -0500

    Impliment concern for default image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   #keep logic in model.  Use lamba fucntion.
@@ -10,8 +11,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     #shortcut for setting image if nil. otherwise it bypasses vs overriding via =
   end
 end
