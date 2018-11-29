1d57525a0c20d7119411ca0d4b74f4276eeaf17c
djunh1 <djunh1@gmail.com>
2018-11-29 17:56:19 -0500

    Add image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,4 +5,13 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> {
     where(subtitle: 'Holy Moly Mike Santolli')
   }
+
+  #Init after new , NOT create
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    #shortcut for setting image if nil. otherwise it bypasses vs overriding via =
+  end
 end
