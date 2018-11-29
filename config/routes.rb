b64cf7158fb78ac3e64f3bcf1694835ceaf8a173
djunh1 <djunh1@gmail.com>
2018-11-28 20:05:49 -0500

    Modify urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs
 
   get 'about', to: 'pages#about'
