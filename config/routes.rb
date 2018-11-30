aa0c27c118c90c82d4a192a28e8f37671bb44eea
djunh1 <djunh1@gmail.com>
2018-11-29 21:41:47 -0500

    add auth to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '',
+              path_names: {sign_in: 'login', sign_out: 'logout' , sign_up: 'register'} 
   root to: 'pages#home'
 
   resources :portfolios, except: [:show]
