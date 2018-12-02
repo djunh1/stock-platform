26eefc40e18df739bbbbecbe88ae1142dae2922a
djunh1 <djunh1@gmail.com>
2018-12-02 12:58:46 -0500

    Add session tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
 
   include DeviseWhitelist
 
+  before_action :set_source
+
+  def set_source
+    #set only if params w/ q
+    session[:source] =params[:q] if params[:q]
+  end
+
 end
