df2fe395a8a5d9389b0ad460d4a723cfff0e5a04
djunh1 <djunh1@gmail.com>
2018-12-04 19:28:02 -0500

    Add before action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
@@ -28,7 +29,7 @@ class PortfoliosController < ApplicationController
 
   def update
     respond_to do |format|
-      @portfolio_item = Portfolio.find(params[:id])
+      set_portfolio_item
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
@@ -38,16 +39,13 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
     3.times { @portfolio_item.technologies.build }
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio item deleted.' }
@@ -62,5 +60,9 @@ class PortfoliosController < ApplicationController
                                       :body,
                                       technologies_attributes: [:name]
                                     )
+ end
+
+ def set_portfolio_item
+   @portfolio_item = Portfolio.find(params[:id])
  end
 end
