e8e9a61a6240ecb8a74caa1b16580f39cfd477cc
djunh1 <djunh1@gmail.com>
2018-12-10 17:54:25 -0500

    Add blog styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
-  def login_helper
+  def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: 'nav-link')+
-      
+
       (link_to "Login", new_user_session_path, class: 'nav-link')
     else
        link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link'
