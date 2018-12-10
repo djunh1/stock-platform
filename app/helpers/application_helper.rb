75ce9ced0a2df37c55183f9c7dd2ea0a22b5f19b
djunh1 <djunh1@gmail.com>
2018-12-10 16:27:34 -0500

    Update layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path)+
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: 'nav-link')+
+      
+      (link_to "Login", new_user_session_path, class: 'nav-link')
     else
-       link_to "Logout", destroy_user_session_path, method: :delete
+       link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link'
      end
   end
 
