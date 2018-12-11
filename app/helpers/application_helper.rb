642eec13abd7e51a9861ca7d26f9e5c5dc770c88
djunh1 <djunh1@gmail.com>
2018-12-11 17:38:20 -0500

    Add portfolio masthead, footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: 'nav-link')+
-
-      (link_to "Login", new_user_session_path, class: 'nav-link')
+      (link_to "Register", new_user_registration_path, class: style) +
+      " <br>".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-       link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link'
-     end
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
 
   def source_helper(layout_name)
