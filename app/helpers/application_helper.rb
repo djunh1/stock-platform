353a44f4c00dc081a2dc910e152676fdd2b7fe5c
djunh1 <djunh1@gmail.com>
2018-12-11 21:35:16 -0500

    refactor navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,58 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
-      " <br>".html_safe +
+      " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
-  end
+end
 
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting from #{session[:source]} , current page #{layout_name}"
       content_tag(:p, greeting, class: "source-greeting")
     end
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper(style, tag_type)
+    nav_links = ''
+
+    nav_items.each do |item|
+      #Keep appending to string
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 
 end
