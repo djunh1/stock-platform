e31e2f2ad2037ea93affd4a465f7908a77856821
djunh1 <djunh1@gmail.com>
2018-12-12 17:00:38 -0500

    add jquery deps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
-//= require jquery3
 //= require popper
 //= require bootstrap
