039ecb363451f7837d86a65df027fc894bfc70d2
djunh1 <djunh1@gmail.com>
2018-12-13 18:39:30 -0500

    Upload forms, refactor placeholder functionality

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,8 +1,5 @@
 module Placeholder
   extend ActiveSupport::Concern
 
-  def self.image_generator(height:, width:)
-    "https://via.placeholder.com/#{height}x#{width}"
 
-  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,11 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
   #pluralize the model referenced.
-  has_many :technologies
+
 
   #collection of items associated with a parent model
   accepts_nested_attributes_for :technologies,
@@ -19,12 +19,4 @@ class Portfolio < ApplicationRecord
     order("position ASC")
   end
 
-  #Init after new , NOT create
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    #shortcut for setting image if nil. otherwise it bypasses vs overriding via =
-  end
 end
