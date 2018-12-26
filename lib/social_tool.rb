d13f002dc2f6104d7e912d1e1aabc5b1c97bc314
djunh1 <djunh1@gmail.com>
2018-12-26 08:34:14 -0500

    Add twitter call

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,7 +7,7 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search("#tesla", result_type: 'recent').take(10).collect do |tweet|
+    client.search("#tesla", result_type: 'recent').take(6).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
