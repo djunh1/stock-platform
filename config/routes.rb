cfcc7c11f5d58ec5a78b1280b40495eda0089842
djunh1 <djunh1@gmail.com>
2018-12-26 10:29:30 -0500

    Create comment broadcast

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :comments
   devise_for :users, path: '',
               path_names: {sign_in: 'login', sign_out: 'logout' , sign_up: 'register'}
   root to: 'pages#home'
@@ -16,6 +15,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
