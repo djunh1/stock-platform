c1d067c5f0ad805777a2b832108459a189094f3a
djunh1 <djunh1@gmail.com>
2018-12-26 09:43:18 -0500

    Build initial comment form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,31 +3,25 @@ class BlogsController < ApplicationController
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.page(params[:page]).per(5)
     @page_title = "The Blog Father"
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
     @page_title = @blog.title
-
+    @seo_keywords = @blog.body
   end
 
- # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -42,8 +36,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -54,8 +46,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
