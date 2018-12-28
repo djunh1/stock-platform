4b1e610471ab51e0bf248dce19a19ae31b217a07
djunh1 <djunh1@gmail.com>
2018-12-28 08:12:33 -0500

    Add radio buttons for status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -79,9 +79,9 @@ class BlogsController < ApplicationController
     end
 
     def blog_params
-      params.require(:blog).permit(:title, :body, :topic_id)
+      params.require(:blog).permit(:title, :body, :topic_id, :status)
     end
-    
+
     def set_sidebar_topics
       @sidebar_topics = Topic.with_blogs
     end
