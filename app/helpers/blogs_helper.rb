82fcb25a53f6cf25afad62e7167734f89aa22f25
djunh1 <djunh1@gmail.com>
2018-12-28 08:26:50 -0500

    Add blog markdown

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,25 @@
 module BlogsHelper
   def gravitar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, laungage)
+      CodeRay.scan(code,laungage).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 end
