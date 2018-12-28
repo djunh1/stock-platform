15be0054f30e765e230aacdd8a59956a64039aad
djunh1 <djunh1@gmail.com>
2018-12-28 07:40:42 -0500

    Build topic views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
-      @blogs = Blog.published.page(params[:page]).per(5)
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
     @page_title = "The Blog Father"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :topics, only: [:index, :show]
   devise_for :users, path: '',
               path_names: {sign_in: 'login', sign_out: 'logout' , sign_up: 'register'}
   root to: 'pages#home'
