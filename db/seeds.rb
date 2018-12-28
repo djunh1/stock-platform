7d8fe4836697f3107fbb024176af3f009715eb4a
djunh1 <djunh1@gmail.com>
2018-12-28 10:47:40 -0500

    udpate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
 
 3.times do |topic|
   Topic.create!(
@@ -22,35 +34,43 @@ puts "3 Topics created"
   )
 end
 
+puts "10 blog posts created"
+
 5.times do |skill|
   Skill.create!(
-    title: "Java #{skill}",
-    percent: 25
+    title: "Rails #{skill}",
+    percent: 15
   )
 end
 
-8.times do |port|
+puts "5 skills created"
+
+8.times do |portfolio_item|
   Portfolio.create!(
-    title: "Fake Title",
-    subtitle: "Bond King" ,
-    body: "Sell Tesla Short." ,
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/350x200"
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
-1.times do |port|
+1.times do |portfolio_item|
   Portfolio.create!(
-    title: "Long LFVN",
-    subtitle: "Holy Moly Mike Santolli" ,
-    body: "100,000 bitcoin." ,
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/350x200"
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
-3.times do |tech|
+puts "9 portfolio items created"
+
+3.times do |technology|
   Portfolio.last.technologies.create!(
-    name: "Tech #{tech}"
+    name: "Technology #{technology}"
   )
 end
+
+puts "3 technologies created"
